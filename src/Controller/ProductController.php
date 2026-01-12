5c2c5c4ef5a4efc853760433cb89cfbc368dc499
alexkerber54190 <alexkerber54190@gmail.com>
2026-01-12 16:57:19 +0000

    :construction: Modification d'une ligne inutile

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Security\Http\Attribute\IsGranted
 
 #[Route('/products')]
 class ProductController extends AbstractController
