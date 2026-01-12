4568a88bae621c66dd3b2dcf7972ed9fddd7e2c1
alexkerber54190 <alexkerber54190@gmail.com>
2026-01-12 23:15:14 +0000

    :sparkles: Implémentation de toute la page d'accueil

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted(new Expression('is_granted("ROLE_ADMIN") or is_granted("ROLE_MANAGER")'))]
 class DashboardController extends AbstractController
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +13,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_dashboard');
+        $user = $this->getUser();
+        if ($user) {
+            if ($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_MANAGER')) {
+                return $this->redirectToRoute('app_dashboard');
+            }
+
+            return $this->redirectToRoute('app_product_index');
         }
 
         return $this->render('home/index.html.twig');
