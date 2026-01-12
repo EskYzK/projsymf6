42597b31850bd064affea655a56ce135bae1643e
alexkerber54190 <alexkerber54190@gmail.com>
2026-01-12 23:37:04 +0000

    :construction: Modification des droits pour certaines pages :sparkles: Ajout des redirections vers des pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,15 +13,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $user = $this->getUser();
-        if ($user) {
-            if ($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_MANAGER')) {
-                return $this->redirectToRoute('app_dashboard');
-            }
-
-            return $this->redirectToRoute('app_product_index');
-        }
-
         return $this->render('home/index.html.twig');
     }
 }
