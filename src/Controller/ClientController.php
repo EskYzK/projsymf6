080388599f3cacbf701403068fb9d87b3df6e9f2
alexkerber54190 <alexkerber54190@gmail.com>
2026-01-12 12:12:05 +0000

    :sparkles: Ajout de l'entité Client et de ses controller, form, repository et templates :construction: Mise à jour du fichier home controller pour gérer la redirection lors de l'arrivée sur le projet et de base.html.twig et l'index.html.twig de home pour la redirection sur le tableau des clients

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientType;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/clients')]
+class ClientController extends AbstractController
+{
+    #[Route('/', name: 'app_client_index', methods: ['GET'])]
+    public function index(ClientRepository $clientRepository): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'clients' => $clientRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($client);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Client ajouté avec succès !');
+            return $this->redirectToRoute('app_client_index');
+        }
+
+        return $this->render('client/form.html.twig', [
+            'client' => $client,
+            'form' => $form,
+            'title' => 'Nouveau Client'
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_client_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Client modifié avec succès !');
+            return $this->redirectToRoute('app_client_index');
+        }
+
+        return $this->render('client/form.html.twig', [
+            'client' => $client,
+            'form' => $form,
+            'title' => 'Modifier le Client'
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_client_delete', methods: ['POST'])]
+    public function delete(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$client->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($client);
+            $entityManager->flush();
+            $this->addFlash('success', 'Client supprimé.');
+        }
+
+        return $this->redirectToRoute('app_client_index');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('base.html.twig');
+    #[Route('/', name: 'app_home')]
+    public function index(
+        UserRepository $userRepo,
+        ProductRepository $productRepo,
+        ClientRepository $clientRepo
+    ): Response {
+        $stats = [
+            'users' => $userRepo->count([]),
+            'products' => $productRepo->count([]),
+            'clients' => $clientRepo->count([]),
+        ];
+
+        $latestClients = $clientRepo->findBy([], ['id' => 'DESC'], 5);
+
+        return $this->render('home/index.html.twig', [
+            'stats' => $stats,
+            'latestClients' => $latestClients
+        ]);
     }
 }
